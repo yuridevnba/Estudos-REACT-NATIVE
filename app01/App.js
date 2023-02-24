f01cd01147d5554e7b7c2ecdf8d257a1842f458d
Yuri12 <yuri.00000830267@unicap.br>
2023-02-24 12:51:08 -0300

    Button. Inserindo botões nos aplicativos - Curso de React Native - Aula 13

--- app01/App.js
+++ app01/App.js
@@ -1,30 +1,26 @@
 
 
 import React from 'react';
-import {Text,View,Image,StyleSheet} from 'react-native';
-//import C1 from './componentes/comp1'
+import {
+
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  Button,
+  Alert
+
+
+} from 'react-native';
 import Estilo from './componentes/Estilos'
 import Caixas   from './componentes/Caixa'
-//import { Component } from 'react/cjs/react.production.min';
-
-/*
- pego os parâmetros e passo para o corpo da função.
-(conjunto de parâmetros)=>{corpo, oq ela vai fazer}
-n=>return n*2 === se tiver só 1 parâmetro não precisa usar ()
-
-(n)=>{return n*2} não está associado a nenhum nome, então como chamar? inline ou associando a uma variável.
-(n1,n2)=>{return n1+n2}
-
-*/
-
-
-   const triplo=n=>n*3
 
 
 
-    const dobro =(n)=>{
-    return n*2
-  }
+
+
+  
+  
 
 export default function App1(){
 
@@ -32,9 +28,12 @@ export default function App1(){
 return(
   <View style={Estilo.conteiner}>
 
+<Button
+title="Mostrar mensagem"
+onPress={()=>Alert.alert('MSG','Olá, Bom Dia Bença!!!')
+}
+/>
 
-<Text>{triplo(2)}</Text>
-   <Text>{dobro(2)}</Text>
 
    <Image
   source={require('./assets/logo.png')}
@@ -42,7 +41,7 @@ return(
    />
 
  
-  
+  {vexibir?<Text>Curso de React Native</Text>:<Text>cfbcursos.com.br</Text>}
    <Text style={Estilo.textoPadrão}>Yuri SIlva</Text>
   
    <Text style={Estilo.textoTitulo}>Unicap.br</Text>
@@ -56,7 +55,7 @@ return(
 const estilos = StyleSheet.create({
 logo:{
 
-  width:100,
+  width:250,
   resizeMode:'contain'
 }
 
