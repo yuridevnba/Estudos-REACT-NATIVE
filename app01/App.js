7ef310b83b18a020ad14ed5c1d66305897fc9475
Yuri12 <yuri.00000830267@unicap.br>
2023-04-27 14:38:47 -0300

    Navigation em React Native #P4 TabNavigation - Curso de React Native - Aula 27

--- app01/App.js
+++ app01/App.js
@@ -5,185 +5,108 @@ import React,{Component} from 'react';
 import {Text, View,StyleSheet, Button} from 'react-native';
 import Estilo from './componentes/Estilos';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-const Pilha=createStackNavigator();
+const Guias = createBottomTabNavigator();
 
 function TelaHome({navigation}){
 
-  return (
+      return (
 
-// eslint-disable-next-line react-native/no-inline-styles
-<View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+     // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
 
-<Text>Tela Home</Text>
-<Text>Indíce: telacanal,telacursos</Text>
-
-<Button
-title='TelaCanal'
-onPress={()=>navigation.navigate('Canal')}
-/>
-
-
-
-<Button
-title='TelaCursos'
-onPress={()=>navigation.navigate('Cursos')}
-/>
-
-
-</View>
+           <Text>Tela Home</Text>
+           <Text>Indíce: telacanal,telacursos</Text>
+     </View>
 
   );
 }
 
 function TelaCanal({navigation}){
-  return (
+       return (
 
-// eslint-disable-next-line react-native/no-inline-styles
-<View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+     // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
 
-<Text>Tela Canal</Text>
-<Text>Indíce: telaHome</Text>
+          <Text>Tela Canal</Text>
+          <Text>Indíce: telaHome</Text>
 
-
-
-<Button
-title='Voltar'
-onPress={()=>navigation.navigate('Home')}
-/>
-
-</View>
+      </View>
 
   );
 }
 
-function TelaCursos({navigation}){
-  return (
+  function TelaCursos({navigation}){
+        return (
 
-// eslint-disable-next-line react-native/no-inline-styles
-<View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+       // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
 
-<Text>Tela Cursos</Text>
+      <Text>Tela Cursos</Text>
 
-
-
-<Button
-title='cursoReactNative'
-onPress={()=>navigation.navigate('CursosReactNative',{aulas:100,autor:'Yuri'})}
-/>
-
-
-
-<Button
-title='Voltar'
-onPress={()=>navigation.goBack()}
-/>
-
-</View>
+       </View>
 
   );
 }
-
-
-
-function TelaCursosReactNative({route,navigation}){
-
-  const {aulas}=route.params;
-  const{autor}=route.params;
-
-  return (
-
-// eslint-disable-next-line react-native/no-inline-styles
-<View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-
-<Text>Curso React Native</Text>
-<Text>Aulas:{aulas}</Text>
-<Text>Autor:{autor}</Text>
-
-
-
-<Button
-title='Voltar'
-onPress={()=>navigation.goBack()}
-/>
-
-<Button
-title='Home'
-onPress={()=>navigation.navigate('Home')}
-/>
-
-</View>
-
-  );
-}
-
 
 
 export default function App1(){
 
-return (
+     return (
 
-   <NavigationContainer>
-  <Pilha.Navigator initialRouteName='TelaHome'>
+      <NavigationContainer>
+      <Guias.Navigator initialRouteName="TelaHome">
 
-  <Pilha.Screen
-  name="Home"
-  component={TelaHome}
-  options={{
-  title:'Tela Inicial',
-  headerStyle:{
-    backgroundColor:'#008'
+      <Guias.Screen
+      name="Home"
+      component={TelaHome}
+      options={{
+      title:'Tela Inicial',
+      headerStyle:{
+      backgroundColor:'#008',
   },
-  headerTintColor:'#fff',
-  headerRight:()=>(
+      headerTintColor:'#fff',
+      headerRight:()=>(
 
-<Button
+   <Button
 
-title='CURSOS'
-color="#000"
-onPress={()=>alert('Botão CUrsos Clicado')}
+     title="CURSOS"
+     color="#000"
+     onPress={()=>alert('Botão CUrsos Clicado')}
 
-/>
-  ),
-  }}
-  />
+    />
+    ),
+    }}
+    />
 
 
-<Pilha.Screen
-  name="Canal"
-  component={TelaCanal}
-  options={{
+  <Guias.Screen
+    name="Canal"
+    component={TelaCanal}
+    options={{
     title:'Tela Canal',
     headerStyle:{
-      backgroundColor:'#008'
+    backgroundColor:'#008',
     },
     headerTintColor:'#fff',
     headerTitleStyle:{
-      fontWeight:'bold'
+      fontWeight:'bold',
 
-    }
+    },
     }}
-  
+
   />
 
 
-<Pilha.Screen
-  name="Cursos"
-  component={TelaCursos}
+  <Guias.Screen
+   name="Cursos"
+   component={TelaCursos}
   options={{title:'Tela de Cursos'}}
   />
 
-<Pilha.Screen
-  name="CursosReactNative"
-  component={TelaCursosReactNative}
-  options={{title:'Curso de Reacat Native'}}
-  />
-
-
-
-  </Pilha.Navigator>
+  </Guias.Navigator>
    </NavigationContainer>
  );
-};
+}
